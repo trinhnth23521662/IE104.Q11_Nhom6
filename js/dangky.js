b945b657d246445ecd4f8834197cc255519a75c8
Tiem <23521566@gm.uit.edu.vn>
2025-11-17 20:38:16 +0700

    Add files via upload

---
+++ js/dangky.js
@@ -0,0 +1,22 @@
+
+  function handleLogin(event) {
+    event.preventDefault(); // Ngăn chuyển trang mặc định
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const confirmPassword = document.getElementById("confirm-password").value.trim();
+
+    if (!email || !password || !confirmPassword) {
+      alert("Vui lòng nhập đầy đủ thông tin!");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      alert("Mật khẩu nhập lại không khớp!");
+      return;
+    }
+
+    // Nếu hợp lệ → chuyển sang index.html
+    window.location.href = "index.html";
+  }
+
